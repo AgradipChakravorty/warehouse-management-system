0b35f21d7c063e2d8f6ae78889802cc472a6fc07
Aanya <singhaanya9887@gmail.com>
2026-05-10 19:20:12 +0530

    feat: implemented global exception handling

--- src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Validation exception handling
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(
+            MethodArgumentNotValidException ex)
+    {
+
+        String errorMessage = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return new ResponseEntity<>(
+                errorMessage,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    // General exception handling
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex)
+    {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
 }
